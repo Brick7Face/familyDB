fdaccde4ddcb76f358cd89313b169cc420118faf
Nate Tranel <nate.tranel@oracle.com>
2020-02-11 15:29:59 -0700

    add age

--- Person.py
+++ Person.py
@@ -1,3 +1,5 @@
+from datetime import date as dt
+
 class Person:
 
     def __init__(self, id, name, parentsMarriageID, dob, dod, birthplace, deathplace):
@@ -14,7 +16,25 @@ class Person:
             self.deathplace = ""
         else:
             self.deathplace = deathplace
+        self.age = self.calcAge(dob, dod)
 
     def displayPerson(self):
-        print("Name:", self.name, "\nBorn:", self.dob, "in", self.birthplace, "\nDied:", self.dod, self.deathplace)
+        print("Name:", self.name, "\nBorn:", self.dob, "in", self.birthplace, "\nDied:", self.dod, self.deathplace, "(Age", str(self.age) + ")")
         print("-----------")
+
+    def calcAge(self, dob, dod):
+        age = None
+        if (dod==None):
+            dobStr = dob.split("-")
+            if (dobStr[0]=='0000'):
+                return age
+            ageDT = dt.today() - dt(int(dobStr[0]), int(dobStr[1]), int(dobStr[2]))
+            age = int(ageDT.days / 365.25)
+        else:
+            dobStr = dob.split("-")
+            dodStr = dod.split("-")
+            if (dobStr[0]=='0000' or dodStr[0]=='0000'):
+                return age
+            ageDT = dt(int(dodStr[0]), int(dodStr[1]), int(dodStr[2])) - dt(int(dobStr[0]), int(dobStr[1]), int(dobStr[2]))
+            age = int(ageDT.days / 365.25)
+        return age
