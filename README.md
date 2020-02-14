7e09451f65eaf0c8563dca27d48aa3d3b71c030e
Nate Tranel <nate.tranel@oracle.com>
2020-02-14 11:23:50 -0700

    full gui - just needs cleanup/refactoring

--- README.md
+++ README.md
@@ -8,6 +8,12 @@
 
 ## Packaging
 To package this program into a simple executable file, install
-PyInstaller fro PyPI: `pip install pyinstaller` and run it using
-`pyinstaller -F -n familyViewer --clean screen.py`. This needs to be done manually on the OS
-it is going to be used on.
+PyInstaller from PyPI: `pip install pyinstaller` and refer to below.
+#### Windows and Linux
+Create the executable file using
+`pyinstaller -F -n familyViewer --clean screen.py`. This needs to be done manually on the OS it is going to be used on.
+#### MacOS
+Instead use the command `pyinstaller -F -n familyViewer --clean --add-binary /System/Library/Frameworks/Tcl.framework/Tcl:Tcl --add-binary /System/Library/Frameworks/Tk.framework/Tk:Tk screen.py`
+on MacOS. This works best on Python 3.7.0, but 3.7.5 will handle it. Any version greater
+than that will crash your computer and send you to the login screen due to a bug with the
+Python Tkinter library and MacOS.
--- screen.py
+++ screen.py
@@ -1,11 +1,23 @@
-import tkinter
+import tkinter as tk
+from tkinter import ttk
+import sys
 import familyDB
+
+class StdRedirector(object):
+    def __init__(self, text_widget):
+        self.text_space = text_widget
+
+    def write(self, string):
+        self.text_space.config(state=tk.NORMAL)
+        self.text_space.insert("end", string)
+        self.text_space.see("end")
+        self.text_space.config(state=tk.DISABLED)
 
 class Screen:
     def __init__(self):
         self.db = familyDB.FamilyDB()
         #create root window
-        self.root = tkinter.Tk()
+        self.root = tk.Tk()
         self.mainMenu()
 
     def clear(self):
@@ -14,44 +26,50 @@ class Screen:
         self.root.quit()
 
     def initFrames(self):
-        self.mid_frame = tkinter.Frame(self.root)
-        self.bottom_frame = tkinter.Frame(self.root)
+        self.mid_frame = tk.Frame(self.root)
+        self.bottom_frame = tk.Frame(self.root)
 
     def mainMenu(self):
         try:
             self.clear()
             self.top_frame.destroy()
+            self.display_frame.destroy()
         except: (AttributeError)
-        self.top_frame = tkinter.Frame(self.root, height = 40)
+        self.top_frame = tk.Frame(self.root, height = 40)
         self.initFrames()
-        #Create two labels
-        self.search_button = tkinter.Button(self.mid_frame, text = 'Search', width = 20, command = self.filterMenu)
-        self.family_button = tkinter.Button(self.mid_frame, text = 'Family', width = 20, command = lambda: self.entryMenu("Enter full name", ""))
-        self.populate_button = tkinter.Button(self.mid_frame, text = 'Populate Database', width = 20, command = self.populate)
-        #Pack top frame widgets
+        self.display_frame = tk.Frame(self.root)
+
+        self.search_button = ttk.Button(self.mid_frame, text = 'Search', width = 20, command = self.filterMenu)
+        self.family_button = ttk.Button(self.mid_frame, text = 'Family', width = 20, command = lambda: self.entryMenu("Enter full name", ""))
+        self.populate_button = ttk.Button(self.mid_frame, text = 'Populate Database', width = 20, command = self.populate)
+        self.quit_button = ttk.Button(self.bottom_frame, text = 'Quit', width = 15, command = self.root.destroy)
+        self.displayBox = tk.Text(self.display_frame, state=tk.DISABLED)
+
+        sys.stdout = StdRedirector(self.displayBox)
+        sys.stderr = StdRedirector(self.displayBox)
+
         self.search_button.pack(side = 'top')
         self.family_button.pack(side = 'top')
         self.populate_button.pack(side = 'bottom')
-        #root.destroy exits/destroys the main window
-        self.quit_button = tkinter.Button(self.bottom_frame, text = 'Quit', width = 15, command = self.root.destroy)
-        #Pack the buttons
         self.quit_button.pack(side = 'left')
+        self.displayBox.pack(side = 'bottom')
 
         #Now pack the frames also
         self.top_frame.pack()
         self.mid_frame.pack()
         self.bottom_frame.pack()
+        self.display_frame.pack()
         self.root.mainloop()
 
     def filterMenu(self):
         self.clear()
         self.initFrames()
 
-        self.name_button = tkinter.Button(self.mid_frame, text = 'Name', width = 20, command = lambda: self.entryMenu("Enter name", "Name"))
-        self.dob_button = tkinter.Button(self.mid_frame, text = 'Birthdate', width = 20, command = lambda: self.entryMenu("Enter birthday YYYY-MM-DD", "Birthday"))
-        self.birthplace_button = tkinter.Button(self.mid_frame, text = 'Birthplace', width = 20, command = lambda: self.entryMenu("Enter birthplace", "Birthplace"))
-        self.deathplace_button = tkinter.Button(self.mid_frame, text = 'Deathplace', width = 20, command = lambda: self.entryMenu("Enter deathplace", "Deathplace"))
-        self.back_button = tkinter.Button(self.bottom_frame, text = 'Return', width = 15, command = self.mainMenu)
+        self.name_button = ttk.Button(self.mid_frame, text = 'Name', width = 20, command = lambda: self.entryMenu("Enter name", "Name"))
+        self.dob_button = ttk.Button(self.mid_frame, text = 'Birthdate', width = 20, command = lambda: self.entryMenu("Enter birthday YYYY-MM-DD", "Birthday"))
+        self.birthplace_button = ttk.Button(self.mid_frame, text = 'Birthplace', width = 20, command = lambda: self.entryMenu("Enter birthplace", "Birthplace"))
+        self.deathplace_button = ttk.Button(self.mid_frame, text = 'Deathplace', width = 20, command = lambda: self.entryMenu("Enter deathplace", "Deathplace"))
+        self.back_button = ttk.Button(self.bottom_frame, text = 'Return', width = 15, command = self.mainMenu)
 
         self.name_button.pack(side = 'top')
         self.dob_button.pack(side = 'top')
@@ -67,14 +85,14 @@ class Screen:
         self.clear()
         self.initFrames()
 
-        self.label = tkinter.Label(self.mid_frame, text = label)
-        self.nameEntry = tkinter.Entry(self.mid_frame)
+        self.label = tk.Label(self.mid_frame, text = label)
+        self.nameEntry = tk.Entry(self.mid_frame)
         if (filter==""):
-            self.submit_button = tkinter.Button(self.mid_frame, text = 'Submit', command = self.family)
-            self.back_button = tkinter.Button(self.bottom_frame, text = 'Return', width = 15, command = self.mainMenu)
+            self.submit_button = ttk.Button(self.mid_frame, text = 'Submit', command = self.family)
+            self.back_button = ttk.Button(self.bottom_frame, text = 'Return', width = 15, command = self.mainMenu)
         else:
-            self.submit_button = tkinter.Button(self.mid_frame, text = 'Search', command = lambda: self.search(filter))
-            self.back_button = tkinter.Button(self.bottom_frame, text = 'Return', width = 15, command = self.filterMenu)
+            self.submit_button = ttk.Button(self.mid_frame, text = 'Search', command = lambda: self.search(filter))
+            self.back_button = ttk.Button(self.bottom_frame, text = 'Return', width = 15, command = self.filterMenu)
 
         self.label.pack(side = 'left')
         self.nameEntry.pack(side = 'right')
